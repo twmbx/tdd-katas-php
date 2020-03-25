4c3b87290f20fafb125d4e21a393cba3d116557f
Twaambo Haamucenje <twaambo@outlook.com>
2020-03-25 03:37:09 +0200

    added secondary features

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -12,10 +12,10 @@ class FizzBuzz
         if (0 === $number % 3 && 0 === $number % 5) {
             return 'FizzBuzz';
         }
-        if (0 === $number % 3) {
+        if (0 === $number % 3 || false !== strpos((string) $number, '3')) {
             return 'Fizz';
         }
-        if (0 === $number % 5) {
+        if (0 === $number % 5 || false !== strpos((string) $number, '5')) {
             return 'Buzz';
         }
         return (string) $number;
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -16,8 +16,14 @@ class FizzBuzzTest extends TestCase
             'Test that 6 returns Fizz' => [6, 'Fizz'],
             'Test that 10 returns Buzz' => [10, 'Buzz'],
             'Test that 15 returns FizzBuzz' => [15, 'FizzBuzz'],
+            'Test that 45 returns FizzBuzz' => [45, 'FizzBuzz'],
             'Test that 101 returns Invalid' => [101, 'Invalid'],
-            'Test that -1 returns Invalid' => [-1, 'Invalid']
+            'Test that -1 returns Invalid' => [-1, 'Invalid'],
+            'Test that 23 returns Fizz' => [23, 'Fizz'],
+            'Test that 38 returns Fizz' => [38, 'Fizz'],
+            'Test that 83 returns Fizz' => [83, 'Fizz'],
+            'Test that 52 returns Buzz' => [52, 'Buzz'],
+            'Test that 25 returns Buzz' => [25, 'Buzz']
         ];
     }
 
